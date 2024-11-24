11db2cbb21f040b3400233a878c43569dc0b8348
Lee Jie Yeng <109010589+JyLee98ImTrying@users.noreply.github.com>
2024-11-24 09:23:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,8 +115,9 @@ def recommend_food(input_data, df, models, excluded_indices=None):
         filtered_df = df.copy()
         if health_condition == "Diabetic":
             filtered_df = filtered_df[
-                (filtered_df['SugarContent'] <= 5)
-            ]
+                (filtered_df['SugarContent'] <= 5) &
+                (filtered_df['RecipeCategory'] != 'Dessert')
+    ]
         elif health_condition == "High Blood Pressure":
             filtered_df = filtered_df[
                 (filtered_df['SodiumContent'] <= 500)  # Max 500mg sodium per serving
