7e4c7e1b956a7a300789e06ed8ed339641c3ec4b
Lee Jie Yeng <109010589+JyLee98ImTrying@users.noreply.github.com>
2024-12-15 09:16:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -409,7 +409,7 @@ if page == "🍅🧀MyHealthMyFood🥑🥬":
         if current_recommendations is not None and not current_recommendations.empty:
             st.write("### 🍳 Recommended Food Items (Single Serving)")
     
-            top_recipes = current_recommendations.head(10)
+            top_recipes = current_recommendations.head(6)
     
             # Display all recommended recipes in expanders initially
             for _, recipe in top_recipes.iterrows():
